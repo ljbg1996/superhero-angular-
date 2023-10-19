5900f03bed2767d96aabc385b04bf2b8d9d68c54
ljbg1 <ljbg1996wang@gmail.com>
2023-10-19 14:51:30 +0200

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SupEditComponent } from "./components/sup-edit/sup-edit.component";
+import { SupListComponent } from "./components/sup-list/sup-list.component";
+import { FightComponent } from "./components/fight/fight.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details/:id', component: SupEditComponent },
+  { path: 'list', component: SupListComponent },
+  { path: 'fight', component: FightComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SupListComponent } from "./components/sup-list/sup-list.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SupEditComponent } from './components/sup-edit/sup-edit.component';
+import { DeadPipe } from './pipes/dead.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { FightComponent } from './components/fight/fight.component';
+import { SuperhumanComponent } from './components/superhuman/superhuman.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SupListComponent,
+    SupEditComponent,
+    DeadPipe,
+    FightComponent,
+    SuperhumanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
